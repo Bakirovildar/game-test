a7fa49ed8a76400ed79faa23ad20931fffcc55ab
Bakirovildar <bakirovildar2011@mail.ru>
2022-10-30 20:25:55 +0500

    сделал стили для BgNewYear

--- styles/styledGamePage.ts
+++ styles/styledGamePage.ts
@@ -12,3 +12,14 @@ export const StyledBg: any = styled.div`
   height: 100%;
   align-items: ${({isTop}: any) => isTop ? 'flex-start' : 'flex-end'};
 `
+
+export const StyledNewYear = styled.div`
+  position: absolute;
+  left: 0;
+  right: 0;
+
+  .toys {
+    display: flex;
+    justify-content: space-between;
+  }
+`
